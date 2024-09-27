84a706258f10bbef5eca8169e9a22c4347c16626
erhaem <jurig.blogger@gmail.com>
2024-09-27 23:58:56 +0700

    chore: reconfig cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,18 +11,10 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
-      platformProxy: {
-        configPath: 'wrangler.toml',
-        environment: undefined,
-        experimentalJsonConfig: false,
-        persist: false,
-      },
-    }),
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
   },
 };
 
